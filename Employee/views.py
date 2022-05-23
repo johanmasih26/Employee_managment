1bf06ca855f45250dfde87018f72c33843390ac1
johan <harryjohn026@gmail.com>
2022-05-23 12:16:31 +0530

    create issue solved

--- Employee/views.py
+++ Employee/views.py
@@ -21,15 +21,18 @@ class EmployeeCreateView(View):
         context = {'form':form}
         return render(request, 'employee_create.html',context)
     def post(self, request, *args, **kwargs):
-        
-        totallength = request.POST['totallength']
         employee = Employee.objects.create(name=request.POST['employee_name'], salary=request.POST['employee_salary'], joining_date=request.POST['employee_joining_date'])
         employee.save()
+        
+                
+        totalfamilymember = request.POST['totalfamilymember']
+        totaloganization = request.POST['totalorganization']
 
         flag = 0
         member = memberrelation = profession = Organizationname = Description = ''
         
-        for i in range(0, int(totallength)+1):
+        for i in range(1, int(totalfamilymember)+1):
+
             if request.POST['member'+str(i)]:
                 member = request.POST['member'+str(i)]
                 flag = 1
@@ -40,6 +43,11 @@ class EmployeeCreateView(View):
             if request.POST['profession'+str(i)]:
                 profession = request.POST['profession'+str(i)]
                 flag = 1
+            if flag == 1:
+                family_detail = FamilyDetail.objects.create(employees = employee, name=member, relation_with_employee = memberrelation, profession=profession)
+                family_detail.save()
+
+        for i in range(1, int(totaloganization)+1):
             if request.POST['Organizationname'+str(i)]:
                 Organizationname = request.POST['Organizationname'+str(i)]
                 flag = 1
@@ -47,12 +55,13 @@ class EmployeeCreateView(View):
                 Description = request.POST['Description'+str(i)]
                 flag = 1
             if flag == 1:
-                family_detail = FamilyDetail.objects.create(employees = employee, name=member, relation_with_employee = memberrelation, profession=profession)
-                family_detail.save()
                 previous_organization = PreviousOrganization.objects.create(employees = employee, organization_name = Organizationname, description=Description)
                 previous_organization.save()
 
+
+
         return redirect('employee_list')
+
 
 class EmployeeDeleteView(View):
     def get(self, request, pk, *args, **kwargs):
@@ -156,7 +165,7 @@ class EmployeeSearchView(View):
             employee_list = Employee.objects.filter(salary = searchInput)
         else:
             employee_list = Employee.objects.filter(name__icontains=searchInput)
-            family_objects =  FamilyDetail.objects.filter(Q(name__icontains=searchInput) ,relation_with_employee='father')
+            family_objects =  FamilyDetail.objects.filter(name__icontains=searchInput ,relation_with_employee='father')
         
         
         
